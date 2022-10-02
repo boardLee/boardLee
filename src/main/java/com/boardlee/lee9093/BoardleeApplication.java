5427350a7b1d4502e395d645a2155703f36373c5
Jung-Kwon Lee <akdirdl@gmail.com>
2022-10-02 21:53:16 +0900

    init: generated Project

---
+++ src/main/java/com/boardlee/lee9093/BoardleeApplication.java
@@ -0,0 +1,13 @@
+package com.boardlee.lee9093;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardleeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardleeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boardlee/lee9093/BoardleeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardlee.lee9093;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardleeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
